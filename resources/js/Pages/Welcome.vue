4fa9a71c9324d2e3600d0dedfdc0103488582f02
-Haru- <52265509+KenPrz@users.noreply.github.com>
2024-06-06 17:10:14 +0800

    chore: Update npm dependencies and add AOS library

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,11 +6,22 @@ import NavBar from '@/Components/NavBar.vue'
     <section>
         <NavBar />
     </section>
-    <section class="relative py-48 bg-cover bg-center h-auto" style="background-image: url('/images/solar-header-bg.jpg');">
+    <section    
+        data-aos="fade-in"
+        data-aos-duration="500" 
+        class="relative py-48 bg-cover bg-center h-auto" style="background-image: url('/images/solar-header-bg.jpg');">
         <div class="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center items-center text-center">
             <div class="flex flex-col items-center justify-center space-y-4">
-                <h1 class="text-4xl text-offset-500">Renewable Energy</h1>
-                <h2 class="text-5xl font-semibold text-white">Shine brighter, Live lighter</h2>
+                <h1
+                data-aos="fade-right"
+                data-aos-delay="300"
+                data-aos-duration="600"
+                class="text-4xl text-offset-500">Renewable Energy</h1>
+                <h2 
+                data-aos="fade-left"
+                data-aos-delay="300"
+                data-aos-duration="600"
+                class="text-5xl font-semibold text-white">Shine brighter, Live lighter</h2>
             </div>
         </div>
     </section>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
 import 'boxicons'
+import 'aos/dist/aos.css'; 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import AOS from 'aos';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
+AOS.init()
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
@@ -16,6 +18,7 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
+
     },
     progress: {
         color: '#4B5563',

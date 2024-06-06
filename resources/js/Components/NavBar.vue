754556ae01c6cf18c82a72c20eb1490d99f7e101
-Haru- <52265509+KenPrz@users.noreply.github.com>
2024-06-06 23:03:39 +0800

    chore: Add SVG wrapper files and implement scroll event listener in Welcome.vue

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -1,58 +1,74 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
 <template>
-    <div>
-        <nav class="flex flex-col">
-            <div class="flex pe-2 md:pe-0 justify-end md:justify-around bg-black text-white w-full text-sm font-light py-1 space-x-1">
-                <div class="flex md:justify-center items-center space-x-1 md:space-x-4">
-                    <a href="" class="flex items-center space-x-2">
-                        <box-icon name='map' color="white" size="sm"></box-icon>
-                        <span class="hidden md:block">6620 Purok 1, San Antonio, Los Baños, Laguna</span>
-                    </a>
-                    <a href="" class="flex items-center space-x-2">
-                        <box-icon name='envelope' color="white" size="sm"></box-icon>
-                        <span class="hidden md:block">mprlsolar@gmail.com</span>
-                    </a>
-                </div>
-                <div class="flex items-center space-x-1 md:space-x-4">
-                    <a href="#" class="flex items-center">
-                        <box-icon name='facebook-circle' type='logo' color="white" size="sm"></box-icon>
-                    </a>
-                    <a href="#" class="flex items-center">
-                        <box-icon name='instagram' type='logo' color="white" size="sm"></box-icon>
-                    </a>
-                </div>
-            </div>
-            <div class="flex justify-around py-1 items-center">
-                <div>
-                    <ApplicationLogo />
-                </div>
-                <div>
-                    <ul class="flex space-x-5 text-lg">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About Us</a>
-                        </li>
-                        <li>
-                            <a href="#">Services</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <button class="bg-primary-500 hover:bg-primary-600 transition-colors duration-200 text-white py-1 px-3 rounded-2xl">
-                        Get a Quote
-                    </button>
-                </div>
-            </div>
-        </nav>
+    <!-- Hide this section when scrolling -->
+    <div
+        class="flex pe-2 md:pe-0 justify-end md:justify-around bg-black text-white w-full text-sm font-light py-1 space-x-1">
+        <div class="flex md:justify-center items-center space-x-1 md:space-x-4">
+            <a href="" class="flex items-center space-x-2">
+                <box-icon name='map' color="white" size="sm"></box-icon>
+                <span class="hidden md:block">6620 Purok 1, San Antonio, Los Baños, Laguna</span>
+            </a>
+            <a href="" class="flex items-center space-x-2">
+                <box-icon name='envelope' color="white" size="sm"></box-icon>
+                <span class="hidden md:block">mprlsolar@gmail.com</span>
+            </a>
+        </div>
+        <div class="flex items-center space-x-1 md:space-x-4">
+            <a href="#" class="flex items-center">
+                <box-icon name='facebook-circle' type='logo' color="white" size="sm"></box-icon>
+            </a>
+            <a href="#" class="flex items-center">
+                <box-icon name='instagram' type='logo' color="white" size="sm"></box-icon>
+            </a>
+        </div>
+    </div>
+    <!-- Make this section sticky when scrolling -->
+    <div class="flex justify-between md:justify-around py-1 items-center h-30">
+        <div class="flex flex-col px-4 md:px-2">
+            <ApplicationLogo />
+            <button
+                class="bg-primary-500 hover:bg-primary-600 transition-colors duration-200 text-white py-1 px-3  md:hidden">
+                Get a Quote
+            </button>
+        </div>
+        <div>
+            <ul class="flex space-x-5 sm:text-sm md:text-md lg:text-lg">
+                <li>
+                    <a class="hidden md:block" href="#">Home</a>
+                    <box-icon class="md:hidden" :color="is_black? 'black':'white'" name='home' ></box-icon>
+                </li>
+                <li>
+                    <a class="hidden md:block" href="#">About Us</a>
+                    <box-icon class="md:hidden" :color="is_black? 'black':'white'" name='buildings' ></box-icon>
+                </li>
+                <li>
+                    <a class="hidden md:block" href="#">Services</a>
+                    <box-icon class="md:hidden" :color="is_black? 'black':'white'" name='wrench' ></box-icon>
+                </li>
+                <li>
+                    <a class="hidden md:block" href="#">Blog</a>
+                    <box-icon class="md:hidden" :color="is_black? 'black':'white'" name='book-open' ></box-icon>
+                </li>
+                <li>
+                    <a class="hidden md:block" href="#">Contact Us</a>
+                    <box-icon class="md:hidden" :color="is_black? 'black':'white'" name='message' ></box-icon>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button
+                class="bg-primary-500 hover:bg-primary-600 transition-colors duration-200 text-white py-1 px-3 rounded-2xl hidden md:block">
+                Get a Quote
+            </button>
+        </div>
     </div>
 </template>
-<script setup>
-    import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+<script>
+    export default {
+        props: {
+            is_black: Boolean
+        }
+    }
 </script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,41 +1,78 @@
 <script setup>
 import NavBar from '@/Components/NavBar.vue'
+import { onMounted, onUnmounted, ref } from 'vue';
+const scroll = ref(0);
+
+const handleScroll = () => {
+    scroll.value = Math.round(window.scrollY);
+};
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
 </script>
-
 <template>
-    <section>
-        <NavBar />
-    </section>
-    <section    
-        data-aos="fade-in"
-        data-aos-duration="500" 
-        class="relative py-48 bg-cover bg-center h-auto" style="background-image: url('/images/solar-header-bg.jpg');">
-        <div class="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center items-center text-center">
-            <div class="flex flex-col items-center justify-center space-y-4">
-                <h1
-                data-aos="fade-right"
-                data-aos-delay="300"
-                data-aos-duration="600"
-                class="text-4xl text-offset-500">Renewable Energy</h1>
-                <h2 
-                data-aos="fade-left"
-                data-aos-delay="300"
-                data-aos-duration="600"
-                class="text-5xl font-semibold text-white">Shine brighter, Live lighter</h2>
+    <nav :class="{
+        'bg-white shadow-md fixed': scroll > 0,
+        'bg-none text-white absolute bg-black bg-opacity-10': scroll === 0
+    }" class="flex flex-col top-0 w-full bg-none z-50 h-30 transition-all duration-500 ease-in-out">
+        <NavBar :is_black="scroll > 0" />
+    </nav>
+    <main>
+        <section data-aos="fade-in" data-aos-duration="500" class="relative py-48 bg-cover bg-center h-auto"
+            style="background-image: url('/images/solar-header-bg.jpg');">
+            <div class="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center items-center text-center">
+                <div class="flex flex-col items-center justify-center space-y-4">
+                    <h1 data-aos="fade-right" data-aos-delay="300" data-aos-duration="600"
+                        class="text-4xl text-offset-500">Renewable Energy</h1>
+                    <h2 data-aos="fade-left" data-aos-delay="300" data-aos-duration="600"
+                        class="text-5xl font-semibold text-white">Shine brighter, Live lighter</h2>
+                </div>
             </div>
-        </div>
-    </section>
-</template>
+        </section>
+        <section class="relative py-48 bg-cover bg-center h-auto"
+            style="background-image: url('/images/mission-vision-bg.jpg');">
+            <div
+                class="absolute inset-0 bg-black bg-opacity-40 flex flex-col md:flex-row justify-center items-center md:px-48 text-white space-y-3 md:space-y-0 md:space-x-3">
+                <div data-aos="fade-left"  data-aos-duration="500" class="px-4 md:w-1/2 flex flex-col items-start">
+                    <h1 class="font-semibold text-left text-3xl py-3">
+                        About MPRL Solar Trading
+                    </h1>
+                    <p data-aos="fade-left" data-aos-delay="25" data-aos-duration="500" class="font-light font-pretty">
+                        MPRL Solar Panel Trading offers hassle-free illumination solutions with our selection of
+                        solar-powered LED lights. Easily install our lights within hours to light up pillars and columns
+                        without the need for electrical wiring. With various types, styles, sizes, and colors available,
+                        find the perfect fit for your space. Embrace sustainability - our solar lights are easy to
+                        install and operate independently from the main electrical supply.
+                    </p>
+                    <div data-aos="fade-left" data-aos-delay="50" data-aos-duration="500" class="mt-4">
+                        <button
+                            class="border py-1 px-2 rounded-xl hover:bg-white hover:text-black transition-all duration-200">
+                            Read More
+                        </button>
+                    </div>
+                </div>
+                <div class="px-4 md:w-1/2 text-center md:text-left">
+                    
+                </div>
+            </div>
 
+        </section>
+    </main>
+</template>
 <script>
 export default {
-
 }
 </script>
 
 <style scoped>
 .bg-cover {
     background-size: cover;
+    height: 100vh;
 }
 
 .bg-center {

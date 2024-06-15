72eca30f5db202bca1f70a55d8a98f294a97ec10
0roc4n <esparragojames@gmail.com>
2024-06-15 10:56:30 +0800

    add grid

--- resources/js/Pages/Product/Components/ProductCard.vue
+++ resources/js/Pages/Product/Components/ProductCard.vue
@@ -9,7 +9,8 @@ import { ref } from 'vue';
             <div class="ml-3 hover:bg-lime-500 px-2 rounded hover:shadow"><a href=""><i class="fa-solid fa-list text-2xl hover:text-white"></i></a></div>
         </div>
                     <span class="grid gap-4 grid-cols-3 grid-rows-3">
-                        <span><div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white  rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
+                        <span>
+                            <div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white  rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
                                 <a href="#">
                                     <img class="w-full h-48" src="https://down-my.img.susercontent.com/file/d6948f5638e9020dee763714f34bdc50" alt="">
                                     <h6 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">3 KW Hybrid Solar</h6>
@@ -41,54 +42,7 @@ import { ref } from 'vue';
                                     </svg>
                                 </a>
                             </div>
-                        </span>
-                        <span><div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white  rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
-                                <a href="#">
-                                    <img class="w-full h-48" src="https://down-my.img.susercontent.com/file/d6948f5638e9020dee763714f34bdc50" alt="">
-                                    <h6 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">3 KW Hybrid Solar</h6>
-                                    
-                                </a>
-                                <p class="mb-3  text-sm font-normal text-gray-700 dark:text-gray-400">Sofar Hybrid Inverter with Wifi 6000 HYD-ES
-                                    335W x 8pcs REC Norway Monocrystalline Solar Panels</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div></span>
-                        <span><div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
-                                <a href="#">
-                                    <img class="w-full h-48" src="https://down-my.img.susercontent.com/file/d6948f5638e9020dee763714f34bdc50" alt="">
-                                    <h6 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">3 KW Hybrid Solar</h6>
-                                    
-                                </a>
-                                <p class="mb-3  text-sm font-normal text-gray-700 dark:text-gray-400">Sofar Hybrid Inverter with Wifi 6000 HYD-ES
-                                    335W x 8pcs REC Norway Monocrystalline Solar Panels</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </span>
-                        <span><div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
-                                <a href="#">
-                                    <img class="w-full h-48" src="https://down-my.img.susercontent.com/file/d6948f5638e9020dee763714f34bdc50" alt="">
-                                    <h6 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">3 KW Hybrid Solar</h6>
-                                    
-                                </a>
-                                <p class="mb-3  text-sm font-normal text-gray-700 dark:text-gray-400">Sofar Hybrid Inverter with Wifi 6000 HYD-ES
-                                    335W x 8pcs REC Norway Monocrystalline Solar Panels</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </span>
+                        </span>    
                         <span>
                             <div class="hover:scale-105 transition-transform duration-300 w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-slate-200 dark:border-gray-700">
                                 <a href="#">
@@ -108,7 +62,7 @@ import { ref } from 'vue';
                         </span>
                         
                     </span>
-                </div>
+     </div>
 </template>    
 <script>
 export default{

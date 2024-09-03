90b5f9d2c6fd8c5d59137529c6fe72e6c44c2dd4
-Haru- <52265509+KenPrz@users.noreply.github.com>
2024-09-03 22:48:26 +0800

    Fixes: Update validation rules for blog post creation and update
    -- fix inconsistent rendering of logo.

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,57 +1,62 @@
 <template>
-    <div class="hidden sm:block">
-      <img
-        v-if="isDark"
-        class="h-20 svg-scale"
-        :src="'images/svg/logo/Horizontal/Horizontal-Logo-Black-01.svg'"
-        alt="Dark Logo"
-      >
-      <img
-        v-else
-        class="h-20 svg-scale"
-        :src="'images/svg/logo/Horizontal/Horizontal-Logo-01.svg'"
-        alt="Light Logo"  
-      >
-    </div>
-    <div class="block sm:hidden">
-        <img
-          v-if="isDark"
-          class="h-12 mobile-svg-scale"
-          :src="'/images/svg/logo/Logomrak/Logomark-Black-01.svg'"
-          alt="Dark Logo"
-        >
-        <img
-          v-else
-          class="h-12 mobile-svg-scale"
-          :src="'/images/svg/logo/Logomrak/Logomark-01.svg'"
-          alt="Light Logo"
-        >
-      </div>
-  </template>
-  
-  <script setup>
-  const props = defineProps({
-    isDark: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-  });
-  </script>
-  
-  <style scoped>
-  .mobile-svg-scale {
-    transform: scale(0.8);
-  }
+  <div class="hidden sm:block">
+    <img
+      v-if="isDark"
+      class="h-20 svg-scale"
+      :src="logoHorizontalDark"
+      alt="Dark Logo"
+    >
+    <img
+      v-else
+      class="h-20 svg-scale"
+      :src="logoHorizontalLight"
+      alt="Light Logo"  
+    >
+  </div>
+  <div class="block sm:hidden">
+    <img
+      v-if="isDark"
+      class="h-12 mobile-svg-scale"
+      :src="logoMarkDark"
+      alt="Dark Logo"
+    >
+    <img
+      v-else
+      class="h-12 mobile-svg-scale"
+      :src="logoMarkLight"
+      alt="Light Logo"
+    >
+  </div>
+</template>
+
+<script setup>
+import logoHorizontalDark from '@/../../public/images/svg/logo/Horizontal/Horizontal-Logo-Black-01.svg';
+import logoHorizontalLight from '@/../../public/images/svg/logo/Horizontal/Horizontal-Logo-01.svg';
+import logoMarkDark from '@/../../public/images/svg/logo/Logomrak/Logomark-Black-01.svg';
+import logoMarkLight from '@/../../public/images/svg/logo/Logomrak/Logomark-01.svg';
+
+const props = defineProps({
+  isDark: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+});
+</script>
+
+<style scoped>
+.mobile-svg-scale {
+  transform: scale(0.8);
+}
+.svg-scale {
+  transform: scale(1.0);
+}
+@media screen and (min-width: 768px){
   .svg-scale {
-    transform: scale(1.0);
+    transform: scale(1.5);
   }
-  @media screen and (min-width: 768px){
-    .svg-scale {
-      transform: scale(1.5);
-    }
-    .mobile-svg-scale {
-      display: none;
-    }
+  .mobile-svg-scale {
+    display: none;
   }
-  </style>
+}
+</style>

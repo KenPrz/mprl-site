85efa2a9ef15cab62b98afba52cfd634c74d781c
Kenprz <kendarylausteroperez@gmail.com>
2025-01-05 01:21:02 +0800

    remove unused CSS and JS files to clean up the project

--- public/build/assets/useFileSizeCheck-B-dVzf1m.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./app-Dvv3Ibiv.js";function c(s=1.5){const e=i(!1),r=i("");return{isFileTooLarge:e,errorMessage:r,checkFileSize:t=>{const l=s*1024*1024;return t.size>l?(e.value=!0,r.value=`File size exceeds ${s} MB limit.`,!1):(e.value=!1,r.value="",!0)}}}export{c as u};

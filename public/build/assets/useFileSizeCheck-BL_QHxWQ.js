d7ffb7900ebff152bc41af7eb915bac726f65bee
Kenprz <kendarylausteroperez@gmail.com>
2025-02-04 23:10:43 +0800

    rebuild assets

---
+++ public/build/assets/useFileSizeCheck-BL_QHxWQ.js
@@ -0,0 +1,1 @@
+import{r as i}from"./app-DBlbD3Xe.js";function c(s=1.5){const e=i(!1),r=i("");return{isFileTooLarge:e,errorMessage:r,checkFileSize:t=>{const l=s*1024*1024;return t.size>l?(e.value=!0,r.value=`File size exceeds ${s} MB limit.`,!1):(e.value=!1,r.value="",!0)}}}export{c as u};
